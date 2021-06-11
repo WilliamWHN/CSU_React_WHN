ab1dc21977333dce32bd167a3243a50d80a5826c
Hausmann <william.hausmann@cpnv.ch>
2021-06-11 10:16:31 +0200

    Create configuration file for axios

--- Api.js
+++ Api.js
@@ -1,5 +1,12 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: `http://localhost:80/`
+  baseURL: `http://localhost:80/`,
+  headers: {
+    Authorization: {
+      toString () {
+        return `Bearer ${localStorage.getItem('token')}`
+      }
+    }
+  }
 });
