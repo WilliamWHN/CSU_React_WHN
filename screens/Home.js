68c73f381550e594fdb332f36e6d68938ec48b19
Hausmann <william.hausmann@cpnv.ch>
2021-06-18 14:05:58 +0200

    Improve home page's UI

--- screens/Home.js
+++ screens/Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import {View, Text, Button} from "react-native";
+import {View, Text, StatusBar, StyleSheet, TouchableOpacity} from "react-native";
 
 export default class HomeScreen extends Component{
     constructor(props){
@@ -9,20 +9,32 @@ export default class HomeScreen extends Component{
     render(){
         return(
             <View>
-                <Text>Home Screen</Text>
-                <Button
-                    title="Consulter"
-                    onPress={() => this.props.navigation.push('Consultations')}
-                />
-                <Button
-                    title="Rapporter"
-                    onPress={() => this.props.navigation.push('Reports')}
-                />
-                <Button
-                    title="Drogues"
-                    onPress={() => this.props.navigation.push('Drugs')}
-                />
+                <TouchableOpacity style={styles.buttons} onPress={() => this.props.navigation.push('Consultations')}>
+                   <Text style={styles.textButtons}>Consulter</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.buttons} onPress={() => this.props.navigation.push('Reports')}>
+                   <Text style={styles.textButtons}>Rapporter</Text>
+                </TouchableOpacity>
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        paddingTop: StatusBar.currentHeight,
+    },
+    buttons:{
+        margin: 12,
+        height: 40,
+        borderRadius: 4,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "green",
+    },
+    textButtons:{
+        color: "white",
+        fontSize: 18,
+    }
+  });
