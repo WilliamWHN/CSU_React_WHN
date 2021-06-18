f8984d39bfda75b80f7d59bdbc7d79db4be1ec6d
Hausmann <william.hausmann@cpnv.ch>
2021-06-18 14:52:33 +0200

    Increase UI of reports page

--- screens/Reports.js
+++ screens/Reports.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import {UserContext} from "../context/userContext.js"
 import {View, Text,TouchableOpacity , StyleSheet, StatusBar, SafeAreaView} from "react-native";
 import API from '../Api.js';
+import { BiLoaderCircle } from "react-icons/bi";
 import CheckCard from '../components/CheckCard'
 
 export default class ReportsScreen extends Component{
@@ -11,24 +12,35 @@ export default class ReportsScreen extends Component{
     constructor(props){
         super(props)
         this.state = {
-            tab: "",
+            tab: "pharma",
+            refreshPage: 0,
+            isLoading: true,
         }
+        this.getMissingChecks = this.getMissingChecks.bind(this)
     }
 
     async componentDidMount(){
+       this.getMissingChecks()
+    }
+
+    getMissingChecks(){
         API.get('api/missingchecks/' + this.context.currentBaseId)
         .then(res => {
             this.setState({
-                tab: "pharma",
+                isLoading: false,
+                pharma: null,
+                nova: null,
+            }),
+            this.setState({
                 pharma: res.data.pharma,
                 nova: res.data.nova,
             })
-            
         })  
     }
 
     render(){
         return(
+            this.state.isLoading ? <View style={styles.loadingTextContainer}><Text style={styles.loadingText}><BiLoaderCircle/> Chargement</Text></View> :
             <SafeAreaView style={styles.container}>
                 <View style={styles.buttonsContainer}>
                     <TouchableOpacity style={styles.buttonShift} onPress={() => {this.setState({tab: "pharma"})}}>
@@ -40,12 +52,12 @@ export default class ReportsScreen extends Component{
                 </View>
                 {
                     this.state.tab === "pharma" && this.state.pharma?.map((check) => (
-                        <CheckCard info={check}/>
+                        <CheckCard info={check} onSubmit={this.getMissingChecks}/>
                     ))
                 }
                 {
                     this.state.tab === "nova" && this.state.nova?.map((check) => (
-                        <CheckCard info={check}/>
+                        <CheckCard info={check} onSubmit={this.getMissingChecks}/>
                     ))
                 }
             </SafeAreaView>
@@ -66,7 +78,12 @@ const styles = StyleSheet.create({
     loadingText:{
         textAlign: 'center',
         fontSize: 40,
-        color: "blue",
+        color: "black",
+    },
+    loadingTextContainer:{
+        justifyContent: 'center', //Centered vertically
+        alignItems: 'center', // Centered horizontally
+        flex:1
     },
     buttonStup:{
         margin: 12,
